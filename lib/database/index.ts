2053061c8d7bec854f36ae9ce1bfe07a99267ea0
rdbmsystem <rdbmsystem@gmail.com>
2024-10-13 10:52:48 +0800

    update index.ts

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,43 +1,22 @@
-import mongoose, { Connection } from "mongoose";
-
-// Define an interface for the cached object
-interface Cached {
-  conn: Connection | null;
-  promise: Promise<Connection> | null;
-}
+import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-// Use globalThis for type-safe global access
-const globalWithMongoose = globalThis as typeof globalThis & {
-  mongoose?: Cached;
-};
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
-// Define cached object or initialize if it doesn't exist
-const cached: Cached = globalWithMongoose.mongoose || {
-  conn: null,
-  promise: null,
-};
-
-export const connectToDatabase = async (): Promise<Connection> => {
+export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  if (!MONGODB_URI) throw new Error("Database connection is missing");
+  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
 
-  // If the promise doesn't exist, create a new one and use the mongoose.connection
   cached.promise =
     cached.promise ||
-    mongoose
-      .connect(MONGODB_URI, {
-        dbName: "evently",
-        bufferCommands: false,
-      })
-      .then((mongooseInstance) => mongooseInstance.connection); // Access the connection object
+    mongoose.connect(MONGODB_URI, {
+      dbName: "evently",
+      bufferCommands: false,
+    });
 
   cached.conn = await cached.promise;
-
-  // Store cached connection globally
-  globalWithMongoose.mongoose = cached;
 
   return cached.conn;
 };
